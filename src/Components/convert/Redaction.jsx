f7bac16c158d5f01cf848dba4b7933f7f1d786d8
Baptiste Nautré <nautre.baptiste@hotmail.com>
2024-08-27 16:49:03 +0200

    add redac & render

---
+++ src/Components/convert/Redaction.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types"
+import { useEffect, useState } from "react"
+
+export const Redaction = ({ redacValue }) => {
+
+    const [textValue, setTextValue] = useState()
+
+    useEffect(() => {
+        redacValue(textValue)
+    }, [textValue, redacValue])
+
+    return (
+        <>
+            <textarea value={textValue} onChange={(e) => setTextValue(e.target.value)} style={{ width: '100%', height: "100%", resize: "none" }}>
+            </textarea>
+        </>
+    )
+}
+
+Redaction.propTypes = {
+    redacValue: PropTypes.string.isRequired
+}
