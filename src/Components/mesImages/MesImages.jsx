a79d21d62c070006ae76b2488362269a5b8357af
unknown <dorine.journet@laposte.net>
2024-08-30 10:54:07 +0200

    [CSS] Mes images

--- src/Components/mesImages/MesImages.jsx
+++ src/Components/mesImages/MesImages.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import CarreGallerie from '../carregallerie/CarreGallerie';
 import Button from '../buttons/Button';
 import { HeaderImages } from '../Header/HeaderImages';
+import { SideBar } from '../SideBar/SideBar';
 import './mesImages.css'
 
 
@@ -14,36 +15,42 @@ function MesImages({
 }) {
 
   return (
-    <div>
-        <HeaderImages handleFutureImg={handleFutureImg}/>
-        <div  className='container'>
-            <div className='previewDiv'>
-                {/* Ici prévisualisation */}
-                {futureImg && (
-                <div>
-                    <h2>Prévisualisation :</h2>
-                    <div className='imgAndButtons'>
-                        <img className="futureImg" src={futureImg.srcImg} alt="Prévisualisation" />
-                        <div className='checkBtn'>
-                            <Button nom='OK' handleImg={handleImg}/>
-                            <Button nom='Annuler' handleCancel={handleCancel}/>
+    <div className='container'>
+        <div>
+            <SideBar/>
+        </div>
+        <div className='containerMesImages'>
+            <HeaderImages handleFutureImg={handleFutureImg}/>
+            <div  className='containerImage'>
+                <div className='previewDiv'>
+                    {/* Ici prévisualisation */}
+                    {futureImg && (
+                    <div>
+                        <h2>Prévisualisation :</h2>
+                        <div className='imgAndButtons'>
+                            <img className="futureImg" src={futureImg.srcImg} alt="Prévisualisation" />
+                            <div className='checkBtn'>
+                                <Button nom='OK' fonction={handleImg}/>
+                                <Button nom='Annuler' fonction={handleCancel}/>
+                            </div>
                         </div>
                     </div>
+                    )}
                 </div>
-                )}
-            </div>
-            <div className='stockDiv'>
-                <h2>Images stockées</h2>
-                <div className='imagesStokedDiv'>
-                    {imgs.map((img) => (
-                        <>
-                            <CarreGallerie key={img.id} srcImg={img.srcImg} nom={img.nom} maxWidth='20em'/>
-                        </>
-                    ))}
+                <div className='stockDiv'>
+                    <h2>Images stockées</h2>
+                    <div className='imagesStokedDiv'>
+                        {imgs.map((img) => (
+                            <>
+                                <CarreGallerie key={img.id} srcImg={img.srcImg} nom={img.nom} maxWidth='20em'/>
+                            </>
+                        ))}
+                    </div>
+                    
                 </div>
-                
             </div>
         </div>
+        
        
 
     </div>
